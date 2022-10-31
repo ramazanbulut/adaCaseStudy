b59c9c664f4df549c8c556db8f98ba2afd6980f2
ramazanbulut <56506578+ramazanbulut@users.noreply.github.com>
2022-10-31 20:21:38 +0300

    remove logs

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ app.use(express.urlencoded({extended: true}))
 app.listen(port);
 console.log("Server running on ",port)
 app.post('/book', function(req, res) {
-    console.log(req.body.Tren.Vagonlar)
+    console.log(req.body)
     let count=req.body.RezervasyonYapilacakKisiSayisi
-    let flexible=req.body.KisilerFarkliVagonlaraYerlestirilebilir
+    const flexible=req.body.KisilerFarkliVagonlaraYerlestirilebilir
     result={RezervasyonYapilabilir:false,YerlesimAyrinti:[]}
 
 
     cars=  req.body.Tren.Vagonlar
-    let filtered_cars=cars.filter((vagon)=>vagon.Kapasite*0.7-vagon.DoluKoltukAdet>count)
+    const filtered_cars=cars.filter((vagon)=>vagon.Kapasite*0.7-vagon.DoluKoltukAdet>count)
 
     if(!flexible && filtered_cars.length<1){ 
         result.RezervasyonYapilabilir=false
@@ -34,7 +34,6 @@ app.post('/book', function(req, res) {
             if(flexible&&vagon.Kapasite*0.7-vagon.DoluKoltukAdet>0 && count>0 ){
                 result.YerlesimAyrinti.push({vagonAdi:vagon.Ad,KisiSayisi:count<vagon.Kapasite*0.7-vagon.DoluKoltukAdet?count:Math.round(vagon.Kapasite*0.7-vagon.DoluKoltukAdet)})
                 count-=count<vagon.Kapasite*0.7-vagon.DoluKoltukAdet?count:Math.round(vagon.Kapasite*0.7-vagon.DoluKoltukAdet)
-                console.log(count)
             }
         }
     })
@@ -45,9 +44,8 @@ app.post('/book', function(req, res) {
         result.RezervasyonYapilabilir=false
         result.YerlesimAyrinti=[]
     }
-    
-    
     res.send(result)
+
 });
 app.get("/",function(req,res){
     res.send("Server Running \n you can access the endpoint via /book")
